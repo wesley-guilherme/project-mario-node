2b35cd086ac3af7d5d1834c72dd98c10c466bc6b
wesley-guilherme <wesley.analistasystem@gmail.com>
2025-08-08 17:42:55 -0300

    Commit Mario inserindo métodos random.

--- src/index.js
+++ src/index.js
@@ -18,7 +18,41 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomBlock() {
+    let random = Math.random()
+    let result
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA"
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break;
+        default:
+            result = "CONFRONTO"
+    }
+
+    return result
+}
+
+async function playRaceEngine(character1, character2) {
+    for (let round = 1; round <= 5; round++) {
+        console.log(`🏁 Rodada ${round}`);
+
+        //sortear bloco
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`);
+    }
+} 
+
+
 (async function main() {
-    console.log("hello")
+    console.log(
+        `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando... \n`
+    );
+   
+ await playRaceEngine(player1, player2);
 })() //função auto invocável
+
  
